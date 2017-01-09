2a9b3995d98315666880853d9c8d7edbe56be0c4
Anil <anil.gudur@clariontechnologies.co.in>
2017-01-09 23:33:06 +0530

    10-egghead-angular-2-load-data-based-on-angular-2-route-params

--- app/contacts/contact.component.js
+++ app/contacts/contact.component.js
@@ -11,16 +11,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var router_1 = require('@angular/router');
 require("rxjs/add/operator/map");
+require("rxjs/add/operator/switchMap");
+require("rxjs/add/operator/startWith");
+var http_1 = require('@angular/http');
 var ContactComponent = (function () {
-    function ContactComponent(route) {
+    function ContactComponent(route, http) {
         this.route = route;
-        this.id = route.params.map(function (p) { return p.id; });
+        this.http = http;
+        var apiUrl = 'https://starwars-json-server-ewtdxbyfdz.now.sh/';
+        //this.id = route.params.map((p:any) => p.id);
+        this.contact$ = route.params
+            .map(function (p) { return p.id; })
+            .switchMap(function (id) { return http.get(apiUrl + 'people/' + id)
+            .map(function (res) { return res.json(); })
+            .map(function (contact) { return Object.assign({}, contact, { image: apiUrl + contact.image }); }); })
+            .startWith({ name: 'Loading...', image: '' });
     }
     ContactComponent = __decorate([
         core_1.Component({
-            template: "\n        contact {{id | async}}\n    "
+            template: "\n        <!-- contact {{id | async}} -->\n        <!-- contact {{contact$ | async}} -->\n        <!-- contact {{(contact$ | async)?.name}} --><!-- '?' mark is for \"Don't load this name if this is NULL\" -->\n        <h2>{{(contact$ | async).name}}</h2>\n        <img [src]=\"(contact$ | async).image\">\n    "
         }), 
-        __metadata('design:paramtypes', [router_1.ActivatedRoute])
+        __metadata('design:paramtypes', [router_1.ActivatedRoute, http_1.Http])
     ], ContactComponent);
     return ContactComponent;
 }());
--- app/contacts/contact.component.ts
+++ app/contacts/contact.component.ts
@@ -1,15 +1,35 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute } from '@angular/router';
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/switchMap";
+import "rxjs/add/operator/startWith";
+
+import { Http } from '@angular/http';
 
 @Component({
     template:`
-        contact {{id | async}}
+        <!-- contact {{id | async}} -->
+        <!-- contact {{contact$ | async}} -->
+        <!-- contact {{(contact$ | async)?.name}} --><!-- '?' mark is for "Don't load this name if this is NULL" -->
+        <h2>{{(contact$ | async).name}}</h2>
+        <img [src]="(contact$ | async).image">
     `
 })
 export class ContactComponent{
-    id;
-    constructor(private route:ActivatedRoute){
-        this.id = route.params.map((p:any) => p.id);
+    //id;
+    contact$;
+    constructor(private route:ActivatedRoute, private http:Http){
+
+        const apiUrl = 'https://starwars-json-server-ewtdxbyfdz.now.sh/';
+
+        //this.id = route.params.map((p:any) => p.id);
+        this.contact$ = route.params
+                        .map((p:any) => p.id)
+                        .switchMap(
+                            id => http.get(apiUrl + 'people/' + id)
+                            .map(res => res.json())
+                            .map(contact => Object.assign({}, contact, {image: apiUrl + contact.image}))
+                        )
+                        .startWith({name: 'Loading...', image: ''});
     }
 }
